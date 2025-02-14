4b7b1c1dfc807a806bb526fd2fba0e2595f51dff
Ivo Severins <52487180+ivoseverins@users.noreply.github.com>
2025-02-14 13:53:53 +0900

    Transform source before geometric hashing
    Using the current transformation.

--- matchpoint/core.py
+++ matchpoint/core.py
@@ -1282,19 +1282,24 @@ class MatchPoint:
             Keyword arguments to pass to the geometric hashing query function.
         """
         hash_table = GeometricHashTable([self.destination], source_vertices=self.source_vertices,
-                                        initial_source_transformation=self.transformation,
+                                        # initial_source_transformation=self.transformation,
                                         number_of_source_bases=20, number_of_destination_bases='all',
                                         tuple_size=tuple_size, maximum_distance_source=maximum_distance_source,
                                         maximum_distance_destination=maximum_distance_destination)
 
+        source = self.transformation(self.source)
+
         if method == 'one_by_one':
-            found_transformation = hash_table.query(self.source, **kwargs)
+            found_transformation = hash_table.query(source, **kwargs)
         elif method == 'abundant_transformations':
-            found_transformation = hash_table.query_tuple_transformations([self.source], **kwargs)
+            found_transformation = hash_table.query_tuple_transformations([source], **kwargs)
         else:
             raise ValueError(f'Unknown method {method}')
 
-        self.transformation = found_transformation
+        if found_transformation is not None:
+            self.transformation += found_transformation
+        else:
+            print('No transformation found')
 
 
     def get_unit(self, space):
